fa90e6edb3a01899ac7e1c7851ef0bab89ab0fa3
kt19906 <katotaicheb738301j@gmail.com>
2023-03-09 13:36:03 +0900

    modified setup

--- setup.py
+++ setup.py
@@ -3,9 +3,20 @@ from setuptools import setup, find_packages
 console_scripts = [
 ]
 
+long_description = """
+
+"""
+
 setup(
-    name='grrmpy',
-    version='1.0',
+    name='mk-blender-scr',
     packages=find_packages(),
+    install_requires=['ase', 'nglview', 'jinja2'], 
+    version='1.0',
+    author='Kato Taisetsu',
+    url='',
+    description='Create a Python script for Blender from Atoms of ASE.',
+    long_description=long_description,
+    long_description_content_type='text/markdown', # 'text/plain', 'text/x-rst', 'text/markdown'
+    keywords='blender ase',
     entry_points={'console_scripts': console_scripts},
 )
