6b3ab3cadb554ca3383af01186ef45468b9f9a0e
kt19906 <katotaicheb738301j@gmail.com>
2023-03-17 16:36:08 +0900

    Create MANIFeST.in

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as requirements_file:
-    install_requirements = requirements_file.read().splitlines()
-
 with open('README.rst',encoding='utf-8') as f:
     long_description = f.read()
-    print(long_description)
-      
+print(find_packages()) 
 setup(
     name='mk-blender-scr',
     packages=find_packages(),
-    install_requires=install_requirements, 
-    version='1.0.3',
+    install_requires=[
+        "ase",
+        "ipywidgets<8.0",
+        "Jinja2",
+        "click",
+        "nglview",
+        "pandas",
+        "ipython",
+        "numpy"
+        ], 
+    version='1.0.8',
     author='Kato Taisetsu',
     url='https://shimi-lab.github.io/mk-blender-scr_Document/',
     description='Create a Python script for Blender from Atoms of ASE.',
